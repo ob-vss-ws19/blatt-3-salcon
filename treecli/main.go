a4b62c5b6806de0fc3d81378835495d5f71f89c8
Junior Contreras <junioralex.contreas@gmail.com>
2019-11-11 09:45:36 +0100

    Initial Commit for CLI

--- treecli/main.go
+++ treecli/main.go
@@ -1,5 +1,44 @@
 package main
 
-func main() {
+import (
+	"flag"
+	"fmt"
+	"github.com/AsynkronIT/protoactor-go/actor"
+	"github.com/AsynkronIT/protoactor-go/examples/remotewatch/messages"
+)
 
+type TreeCliActor struct {}
+
+func (state *TreeCliActor) Receive(context actor.Context) {
+	switch msg := context.Message().(type) {
+	case *messages.HelloRequest:
+		fmt.Println(msg);
+
+		//TODO
+	}
+}
+
+//Global Variables
+var (
+	id        *int
+	token     int
+	pid       *actor.PID
+	remotePid *actor.PID
+)
+
+// Command Line Interface
+func main() {
+	fmt.Println("Hello Tree-CLI!")
+	//Using Flags for Input
+	//Define Flags
+	flag.Usage = func() {
+		fmt.Println("This is not helpful")
+	}
+
+	flag.Parse() //after defining all flags, call is necessary
+
+
+
+
+	fmt.Println("Bye Tree-CLI!")
 }
