7f1f3a58976adc7fb01fbed77478ae6acfdf29b4
Salih Uysal <esaeliha@gmail.com>
2019-11-18 21:32:08 +0100

    Initial commit for TreeService

--- treeservice/main.go
+++ treeservice/main.go
@@ -2,17 +2,39 @@ package main
 
 import (
 	"blatt-3-salcon/tree"
+	"crypto/rand"
+	"flag"
+	"fmt"
 	"github.com/AsynkronIT/protoactor-go/actor"
+	"github.com/AsynkronIT/protoactor-go/remote"
+	"sync"
 	"time"
 )
 
-func main() {
-	context := actor.EmptyRootContext
-	props := actor.PropsFromProducer(func() actor.Actor {
-		return &tree.Node{}
-	})
-	pid := context.Spawn(props)
-	context.Send(pid, &tree.Find{Key: 2, RequestFrom: pid})
+type TreeServiceActor struct {
+}
 
-	time.Sleep(2 * time.Second)
+func (state *TreeServiceActor) Receive(context actor.Context) {
+
+}
+
+func getTreeSericeActor() actor.Actor {
+	fmt.Printf("# Tree-Service-Actor is ready\n")
+	return &TreeServiceActor{}
+}
+
+func newToken() string {
+	bytes := make([]byte, 4)
+	rand.Read(bytes)
+	return fmt.Sprintf("%x", bytes)
+}
+
+func main() {
+	var bind = flag.String("bind", "localhost:8088", "Bind to address")
+	var wg sync.WaitGroup
+	wg.Add(1)
+	defer wg.Wait()
+	flag.Parse()
+	remote.Start(*bind)
+	remote.Register("TreeServiceActor", actor.PropsFromProducer(getTreeSericeActor))
 }
