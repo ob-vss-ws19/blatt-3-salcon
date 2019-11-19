225c1663be07e33592b6490fb978aab46b5c879b
Salih Uysal <esaeliha@gmail.com>
2019-11-19 12:05:12 +0100

    Added Node Tests and fixed bugs

--- tree/node.go
+++ tree/node.go
@@ -50,17 +50,18 @@ type Traverse struct {
 // Implementiere Receive
 func (state *Node) Receive(context actor.Context) {
 	switch msg := context.Message().(type) {
+
 	case *Add:
+		// If there is Room in Leaf
 		if (len(state.Data) < state.LeafSize || state.Data[msg.Key] != "") && state.LeftNode == nil && state.RightNode == nil {
-			// add key to leaf
+			// Add a map to leaf
 			if state.Data == nil {
 				state.Data = make(map[int]string)
 			}
 			state.Data[msg.Key] = msg.Val
-			fmt.Printf("added key: %d\n", msg.Key)
-
-		} else if len(state.Data) == 0 && state.LeftNode != nil && state.RightNode != nil {
-			// not a leaf
+			fmt.Printf("\n ### ADD: Added key: %d\n", msg.Key)
+		} else if state.LeftNode != nil && state.RightNode != nil && len(state.Data) == 0 {
+			// If left & right nodes are already defined
 			if msg.Key <= state.MaxKeyVal {
 				// add left
 				context.Send(state.LeftNode, msg)
@@ -69,8 +70,8 @@ func (state *Node) Receive(context actor.Context) {
 				context.Send(state.RightNode, msg)
 			}
 		} else if len(state.Data) == state.LeafSize && state.LeftNode == nil && state.RightNode == nil {
-			// leaf full create new leafs
-			fmt.Println("created new leafs")
+			// If left & right nodes are not defined
+			fmt.Printf("\n ### ADD: Create new Leafs Left & Right\n")
 			props := actor.PropsFromProducer(func() actor.Actor {
 				return &Node{LeafSize: int(state.LeafSize)}
 			})
@@ -78,20 +79,20 @@ func (state *Node) Receive(context actor.Context) {
 
 			state.RightNode = context.Spawn(props)
 
-			// send values to leafs
 			state.Data[msg.Key] = msg.Val
 			keys := sortKeys(state.Data)
 			state.MaxKeyVal = keys[(len(keys)/2)-1]
-			fmt.Printf("left max key %d\n", state.MaxKeyVal)
+			fmt.Printf("\n ### ADD: Maximum Key Value at left %d\n", state.MaxKeyVal)
+			// Values will be sended to leafs
 			for _, key := range keys {
 				if key <= state.MaxKeyVal {
 					// add half left
-					fmt.Printf("send %d left\n", key)
+					fmt.Printf("\n ### ADD: Send Key %d to left leaf\n", msg.Key)
 					context.Send(state.LeftNode, &Add{Key: key, Val: state.Data[key]})
 					delete(state.Data, key)
 				} else {
 					// add half right
-					fmt.Printf("send %d right\n", key)
+					fmt.Printf("\n ### ADD: Send Key %d to right leaf\n", msg.Key)
 					context.Send(state.RightNode, &Add{Key: key, Val: state.Data[key]})
 					delete(state.Data, key)
 				}
@@ -99,7 +100,7 @@ func (state *Node) Receive(context actor.Context) {
 		}
 
 	case *Find:
-		fmt.Printf("\n# FIND: Got Request for %d\n\n", msg.Key)
+		fmt.Printf("\n### FIND: Got Request for %d\n", msg.Key)
 		// Look, it the next nodes have the key, because Datalength is 0
 		if state.LeftNode != nil && state.RightNode != nil && len(state.Data) == 0 {
 			if msg.Key <= state.MaxKeyVal {
@@ -110,23 +111,23 @@ func (state *Node) Receive(context actor.Context) {
 				context.Send(state.RightNode, msg)
 			}
 		} else if state.LeftNode == nil && state.RightNode == nil {
-			fmt.Printf("\n# FIND: Searching for Key %d\n", msg.Key)
-			fmt.Print("\n# FIND: Searching in map: ")
+			fmt.Printf("\n### FIND: Searching for Key %d\n", msg.Key)
+			fmt.Print("\n### FIND: Searching in map: ")
 			fmt.Println(state.Data)
 
 			foundData := state.Data[msg.Key]
-			fmt.Printf("\n# FIND: Data found %s\n", foundData)
+			fmt.Printf("\n### FIND: Data found %s\n", foundData)
 
 			if foundData != "" {
 				context.Send(msg.RequestFrom, &messages.Response{Key: int32(msg.Key), Value: foundData, Type: messages.FIND})
-				fmt.Printf("# FIND: Key %d found\n", msg.Key)
+				fmt.Printf("### FIND: Key %d found\n", msg.Key)
 			}
 		} else {
-			context.Send(msg.RequestFrom, &messages.Error{Message: "# FIND: Key not found"})
+			context.Send(msg.RequestFrom, &messages.Error{Message: "### FIND: Key not found"})
 		}
 
 	case *Remove:
-		fmt.Printf("\n# FIND: Got Request for %d\n\n", msg.Key)
+		fmt.Printf("\n### REMOVE: Got Request for %d\n", msg.Key)
 		// Look, it the next nodes have the key, because Datalength is 0
 		if state.LeftNode != nil && state.RightNode != nil && len(state.Data) == 0 {
 			if msg.Key <= state.MaxKeyVal {
@@ -137,19 +138,19 @@ func (state *Node) Receive(context actor.Context) {
 				context.Send(state.RightNode, msg)
 			}
 		} else if state.LeftNode == nil && state.RightNode == nil {
-			fmt.Printf("\n# FIND: Searching for Key %d\n", msg.Key)
-			fmt.Print("\n# FIND: Searching in map: ")
+			fmt.Printf("\n### REMOVE: Searching for Key %d\n", msg.Key)
+			fmt.Print("\n### REMOVE: Searching in map: \n")
 			fmt.Println(state.Data)
 
 			foundData := state.Data[msg.Key]
-			fmt.Printf("\n# FIND: Data found %s\n", foundData)
+			fmt.Printf("\n### REMOVE: Data found %s\n", foundData)
 
 			if foundData != "" {
 				delete(state.Data, msg.Key)
-				fmt.Printf("deleted key %d\n", msg.Key)
+				fmt.Printf("\n REMOVE: deleted key %d\n", msg.Key)
 			}
 		} else {
-			context.Send(msg.RequestFrom, &messages.Error{Message: "# FIND: Key not found"})
+			context.Send(msg.RequestFrom, &messages.Error{Message: "### REMOVE: Key not found"})
 		}
 
 	case *Traverse:
@@ -161,20 +162,20 @@ func (state *Node) Receive(context actor.Context) {
 			msg.Start = nil
 			if state.LeftNode == nil && state.RightNode == nil {
 				// if root is leaf create slices and set start to nil
-				fmt.Println("send to start")
+				fmt.Printf("\n ### TRAVERSE: SEND TO START\n")
 				context.Send(tmp, msg)
 				return
 			}
-
-			// if root is node create slices, set start to nil, add right node to remaining and forward
+			// if root is node, create slices, set start to nil, add right node to remaining and forward
 			msg.RemainingNodes = append(msg.RemainingNodes, state.RightNode)
-			fmt.Println("send to left node from start")
+			fmt.Printf("\n ### TRAVERSE: SEND TO LEFT NODE FROM START\n")
 			context.Send(state.LeftNode, msg)
 			return
 		} else if state.LeftNode != nil && state.RightNode != nil {
 			// node is not leaf
 			// while remaining nodes add right node to remaining and send to left node
-			fmt.Printf("add right node send to left\n")
+			fmt.Printf("\n ### TRAVERSE: ADD RIGHT NODE SEND TO LEFT\n")
+
 			msg.RemainingNodes = append(msg.RemainingNodes, state.RightNode)
 			context.Send(state.LeftNode, msg)
 		} else if len(msg.RemainingNodes) != 0 && state.LeftNode == nil && state.RightNode == nil {
@@ -187,12 +188,12 @@ func (state *Node) Receive(context actor.Context) {
 			sort.Ints(keys)
 
 			for _, key := range keys {
-				fmt.Printf("appending %d\n", key)
+				fmt.Printf("\n ### TRAVERSE: appending %d\n", key)
 				msg.Values = append(msg.Values, KeyValuePair{key, state.Data[key]})
 			}
 			next := msg.RemainingNodes[len(msg.RemainingNodes)-1]
 			msg.RemainingNodes = msg.RemainingNodes[:len(msg.RemainingNodes)-1]
-			fmt.Println("send to next node from leaf")
+			fmt.Println("\n ### TRAVERSE: send to next node from leaf")
 			context.Send(next, msg)
 		} else if len(msg.RemainingNodes) == 0 && state.LeftNode == nil && state.RightNode == nil {
 			// leaf with no remaining nodes to traverse
@@ -204,23 +205,24 @@ func (state *Node) Receive(context actor.Context) {
 			sort.Ints(keys)
 
 			for _, key := range keys {
-				fmt.Printf("appending %d in last leaf\n", key)
+				fmt.Printf("\n ### TRAVERSE: appending %d in last leaf\n", key)
 				msg.Values = append(msg.Values, KeyValuePair{key, state.Data[key]})
 			}
 
-			respon := make([]*messages.Response, 0)
+			response := make([]*messages.Response, 0)
 
-			fmt.Println("send to caller")
+			fmt.Println("\n ### TRAVERSE: send to caller")
 			for _, pair := range msg.Values {
-				respon = append(respon, &messages.Response{Value: pair.Value, Key: int32(pair.Key)})
+				response = append(response, &messages.Response{Value: pair.Value, Key: int32(pair.Key)})
 			}
-			context.Send(msg.Caller, &messages.Traverse{Values: respon})
+			context.Send(msg.Caller, &messages.Traverse{Values: response})
 		} else {
-			fmt.Printf("error in traverse\n")
+			fmt.Printf("\n ### TRAVERSE: error in traverse\n")
 			context.Send(msg.Caller, &messages.Error{"Error while traversing"})
 		}
+
 	case *Delete:
-		fmt.Println("stopping node")
+		fmt.Println("\n ### DELETE: DELETING NODE")
 		if state.LeftNode != nil {
 			context.Send(state.LeftNode, &Delete{CurrentNode: state.LeftNode})
 		}
@@ -228,11 +230,9 @@ func (state *Node) Receive(context actor.Context) {
 		if state.RightNode != nil {
 			context.Send(state.RightNode, &Delete{CurrentNode: state.RightNode})
 		}
-
 		context.Stop(msg.CurrentNode)
 		fmt.Println("still running?")
 	}
-
 }
 
 func sortKeys(Values map[int]string) []int {
